6eafa32e4e7c753f265afc058d6a2401db19953a
Mohamed Khater <mhmedkhaater@gmail.com>
2024-04-25 00:27:27 +0200

    CaseStudy

--- app/View/Components/Frontend/Projects/viewProjects.php
+++ app/View/Components/Frontend/Projects/viewProjects.php
@@ -10,7 +10,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class viewProjects extends Component
+class ViewProjects extends Component
 {
     /**
      * Create a new component instance.

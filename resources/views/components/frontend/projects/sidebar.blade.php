223bbe6c9989c76ee0deca2f4df704d673a023b6
Mohamed Khater <121749484+Mohammed-Mahmmud@users.noreply.github.com>
2024-05-04 13:27:58 +0300

    projects sidebar

---
+++ resources/views/components/frontend/projects/sidebar.blade.php
@@ -0,0 +1,30 @@
+@foreach ( $pages as $page)
+   <a class="ref_styles active_ref dropdown_arrow 
+   @if(Request::is('en/Projects/'.$page->slug.'/*'))
+   active
+  @endif
+   " href="#">
+                 <div class="img_link">
+                     <img class="Identity_icon" src="content/images/small_icon/card.png" alt />{{  $page->name }}
+                 </div>
+                 <i class="bi bi-chevron-down"></i>
+             </a>
+
+             <ul class="aside_menu">
+             @foreach ($projects as $project )
+                 @if($page->id == $project->Page->id)
+                 {{--  <li class="li_active">{{ $project->title}}</li>  --}}
+                 <a href="{{route('projects',[$page->slug,$project->id])}}">
+                 <li 
+                   @if(Request::is('en/Projects/'.$page->slug.'/'.$project->id))
+                 class="li_active" @else style="color:black"
+                 @endif  
+                 >
+                 {{$project->title }}
+               </li>
+                 </a>
+                 @endif
+             @endforeach
+             </ul>
+
+@endforeach

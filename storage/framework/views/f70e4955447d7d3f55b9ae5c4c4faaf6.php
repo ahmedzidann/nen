7e232e5c5e8cb9726ecaa4a8a7122e9c249ea1b8
ahmedzidann <55232868+ahmedzidann@users.noreply.github.com>
2024-09-14 13:57:24 +0300

    Revert "Design"

---
+++ storage/framework/views/f70e4955447d7d3f55b9ae5c4c4faaf6.php
@@ -0,0 +1,3 @@
+
+<textarea rows="4"   id="<?php echo e($id??''); ?>" class="form-control" placeholder="<?php echo e($placeholder??''); ?>"  <?php echo e($required ?? ''); ?> name="<?php echo e(($name)??''); ?>" /><?php echo e(isset($value) ? $value : old($old??'')); ?></textarea>
+<?php /**PATH D:\xampp8.2\htdocs\nen\resources\views/components/admin/form/text.blade.php ENDPATH**/ ?>

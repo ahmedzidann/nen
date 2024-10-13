03560d33aa8d991e0cae6231dddf4442df1e97c0
omarYasserAli <3omaryasser74@gmail.com>
2024-10-13 13:59:53 +0300

    edits

--- app/Http/Controllers/User/Projects/ProjectController.php
+++ app/Http/Controllers/User/Projects/ProjectController.php
@@ -15,12 +15,13 @@ class ProjectController extends Controller
     public function index($slug= null,$id=null):View
     {
         $page = Page::findOrFail(request()->page_id);
+        $slider = Slider::where('page_id', $page->id)->first();
         $tabs = Tabs::where('type','project')->get();
         $slug = $page->slug;
         $id = $page->id;
         $projects = Project::findOrFail(request()->project_id);
         if(isset($slug)){
-            return view('user.projects.viewProjects',compact('slug','id','projects','page','tabs'));
+            return view('user.projects.viewProjects',compact('slug','id','projects','page','tabs','slider'));
 
         }else abort(400, "error");
         //adadad

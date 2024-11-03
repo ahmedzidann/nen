a70b726fd5755c59a86b096852a0e2a5e7805767
omar95-pero <omarpero85@gmail.com>
2024-11-03 18:08:50 +0200

    fix tabs in investors

--- resources/views/user/about/companies/companies.blade.php
+++ resources/views/user/about/companies/companies.blade.php
@@ -1,50 +1,48 @@
-<div class="row g-3">
-    @forelse ($rows as $sub)
-        <div class="col-lg-4 col-md-6 col-sm-12 col-12">
-            <div class="subsidiaries_content">
-                <div class="first">
-                    <h5 class="title">{{ $sub->translate('title', app()->getLocale()) }}</h5>
-                    <div class="d-flex align-items-center justify-content-center">
-                        <div class="flg_div">
-                            <img src="{{ $sub->getFirstMediaUrl('StaticTable') }}" loading="lazy"
-                                onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';"
-                                alt="vector">
-                        </div>
-                    </div>
-                    <div class="subsidiaries_details">
-                        <div class="flags_sec">
-                            @foreach ($sub->investorAttributes as $row)
-                                <div class="flag_icon_titel">
-                                    <div class="sub_contennt">
-                                        <h6>
-                                            <img src="{{ App\Models\Country::where('id', $row->country_id)->first()->getFirstMediaUrl('flag') }}"
-                                                loading="lazy"
-                                                onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';"
-                                                alt="vector">
-                                            <p>Since : <span
-                                                    class="mt-0 pt-0">{{ $row->since }}</span>
-                                            </p>
-                                        </h6>
-                                        <h6>
-                                            <p>Sharing : <span
-                                                    class="mt-0 pt-0">{{ $row->percent }}%</span>
-                                            </p>
-                                        </h6>
-                                    </div>
-
-                                </div>
-                            @endforeach
-
-                        </div>
+@forelse ($rows as $sub)
+    <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+        <div class="subsidiaries_content">
+            <div class="first">
+                <h5 class="title" style="text-align: center !important;">
+                    {{ $sub->translate('title', app()->getLocale()) }}</h5>
+                <div class="d-flex align-items-center justify-content-center">
+                    <div class="flg_div">
+                        <img src="{{ $sub->getFirstMediaUrl('StaticTable') }}" loading="lazy"
+                            onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';"
+                            alt="vector">
                     </div>
                 </div>
-                <a href="{{ $sub->url }}"
-                    class="website_link mt-0 p-2 text-center">Website</a>
+                <div class="subsidiaries_details">
+                    <div class="flags_sec align-items-start">
+                        @foreach ($sub->investorAttributes as $row)
+                            <div class="flag_icon_titel">
+                                <div class="sub_contennt">
+                                    <h6>
+                                        <img src="{{ App\Models\Country::where('id', $row->country_id)->first()->getFirstMediaUrl('flag') }}"
+                                            loading="lazy"
+                                            onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';"
+                                            alt="vector">
+                                        <p>Since : <span
+                                                class="mt-0 pt-0">{{ $row->since }}</span>
+                                        </p>
+                                    </h6>
+                                    <h6>
+                                        <p>Sharing : <span
+                                                class="mt-0 pt-0">{{ $row->percent }}%</span>
+                                        </p>
+                                    </h6>
+                                </div>
 
+                            </div>
+                        @endforeach
+
+                    </div>
+                </div>
             </div>
-        </div>
-    @empty
-        @include('user.layout.includes.no-data')
-    @endforelse
+            <a href="{{ $sub->url }}"
+                class="website_link mt-0 p-2 text-center">Website</a>
 
-</div>
+        </div>
+    </div>
+@empty
+    @include('user.layout.includes.no-data')
+@endforelse
--- resources/views/user/about/investors.blade.php
+++ resources/views/user/about/investors.blade.php
@@ -69,9 +69,9 @@
             <!-- End Investors Statisctics Section -->
 
             <div class="investors_img mt-md-5 mt-3">
-                    <img src="{{asset('content/images/investors-statistics.jpg') }}" loading="lazy"
-                        onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';">
-                </div>
+                <img src="{{ asset('content/images/investors-statistics.jpg') }}" loading="lazy"
+                    onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';">
+            </div>
         @endif
 
         <!-- Start Partner Section -->
@@ -79,7 +79,8 @@
             <ul class="nav nav-tabs" id="myTab" role="tablist">
                 <li class="nav-item" role="presentation">
                     <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane"
-                        type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true" data-id="subsidiaries">
+                        type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true"
+                        data-id="subsidiaries">
                         <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1"
                             id="Layer_1" class="sparkle svg-with-gap">
                             <path
@@ -93,7 +94,8 @@
                 </li>
                 <li class="nav-item" role="presentation">
                     <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane"
-                        type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false" data-id="sister_companies">
+                        type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false"
+                        data-id="sister_companies">
                         <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1"
                             id="Layer_1" class="sparkle svg-with-gap">
                             <path
@@ -113,43 +115,50 @@
                         <!-- <div class="subsidiaries__sec"> -->
                         <div class="row g-3">
                             @forelse ($rows as $sub)
-                            <div class="col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="subsidiaries_content">
-                                <div class="first">
-                                    <h5 class="title" style="text-align: center !important;">{{ $sub->translate('title', app()->getLocale()) }}</h5>
-                                    <div class="d-flex align-items-center justify-content-center">
-                                    <div class="flg_div">
-                                        <img src="{{ $sub->getFirstMediaUrl('StaticTable') }}" loading="lazy"
-                                            onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';"
-                                            alt="vector">
-                                    </div>
-                                    </div>
-                                    <div class="subsidiaries_details">
-                                        <div class="flags_sec align-items-start">
-                                            @foreach ($sub->investorAttributes as $row)
-                                                <div class="flag_icon_titel">
-                                                    <div class="sub_contennt">
-                                                        <h6>
-                                                            <img src="{{ App\Models\Country::where('id', $row->country_id)->first()->getFirstMediaUrl('flag') }}" loading="lazy"
-                                                                onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';"
-                                                                alt="vector">
-                                                            <p>Since : <span class="mt-0 pt-0">{{ $row->since }}</span></p>
-                                                        </h6>
-                                                        <h6>
-                                                            <p>Sharing : <span class="mt-0 pt-0">{{ $row->percent }}%</span></p>
-                                                        </h6>
-                                                    </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+                                    <div class="subsidiaries_content">
+                                        <div class="first">
+                                            <h5 class="title" style="text-align: center !important;">
+                                                {{ $sub->translate('title', app()->getLocale()) }}</h5>
+                                            <div class="d-flex align-items-center justify-content-center">
+                                                <div class="flg_div">
+                                                    <img src="{{ $sub->getFirstMediaUrl('StaticTable') }}" loading="lazy"
+                                                        onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';"
+                                                        alt="vector">
+                                                </div>
+                                            </div>
+                                            <div class="subsidiaries_details">
+                                                <div class="flags_sec align-items-start">
+                                                    @foreach ($sub->investorAttributes as $row)
+                                                        <div class="flag_icon_titel">
+                                                            <div class="sub_contennt">
+                                                                <h6>
+                                                                    <img src="{{ App\Models\Country::where('id', $row->country_id)->first()->getFirstMediaUrl('flag') }}"
+                                                                        loading="lazy"
+                                                                        onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';"
+                                                                        alt="vector">
+                                                                    <p>Since : <span
+                                                                            class="mt-0 pt-0">{{ $row->since }}</span>
+                                                                    </p>
+                                                                </h6>
+                                                                <h6>
+                                                                    <p>Sharing : <span
+                                                                            class="mt-0 pt-0">{{ $row->percent }}%</span>
+                                                                    </p>
+                                                                </h6>
+                                                            </div>
+
+                                                        </div>
+                                                    @endforeach
 
                                                 </div>
-                                            @endforeach
-
+                                            </div>
                                         </div>
-                                    </div>
-                                    </div>
-                                    <a href="{{ $sub->url }}" class="website_link mt-0 p-2 text-center">Website</a>
+                                        <a href="{{ $sub->url }}"
+                                            class="website_link mt-0 p-2 text-center">Website</a>
 
+                                    </div>
                                 </div>
-                            </div>
                             @empty
                                 @include('user.layout.includes.no-data')
                             @endforelse
@@ -160,45 +169,52 @@
                     tabindex="0">
                     <div class="subsidiaries_sec_content">
                         <!-- <div class="subsidiaries__sec"> -->
-                        <div class="row g-3">
+                        <div class="row g-3" id="container">
                             @forelse ($rows as $sub)
-                            <div class="col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="subsidiaries_content">
-                                <div class="first">
-                                    <h5 class="title" style="text-align: center !important;">{{ $sub->translate('title', app()->getLocale()) }}</h5>
-                                    <div class="d-flex align-items-center justify-content-center">
-                                    <div class="flg_div">
-                                        <img src="{{ $sub->getFirstMediaUrl('StaticTable') }}" loading="lazy"
-                                            onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';"
-                                            alt="vector">
-                                    </div>
-                                    </div>
-                                    <div class="subsidiaries_details">
-                                        <div class="flags_sec align-items-start">
-                                            @foreach ($sub->investorAttributes as $row)
-                                                <div class="flag_icon_titel">
-                                                    <div class="sub_contennt">
-                                                        <h6>
-                                                            <img src="{{ App\Models\Country::where('id', $row->country_id)->first()->getFirstMediaUrl('flag') }}" loading="lazy"
-                                                                onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';"
-                                                                alt="vector">
-                                                            <p>Since : <span class="mt-0 pt-0">{{ $row->since }}</span></p>
-                                                        </h6>
-                                                        <h6>
-                                                            <p>Sharing : <span class="mt-0 pt-0">{{ $row->percent }}%</span></p>
-                                                        </h6>
-                                                    </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+                                    <div class="subsidiaries_content">
+                                        <div class="first">
+                                            <h5 class="title" style="text-align: center !important;">
+                                                {{ $sub->translate('title', app()->getLocale()) }}</h5>
+                                            <div class="d-flex align-items-center justify-content-center">
+                                                <div class="flg_div">
+                                                    <img src="{{ $sub->getFirstMediaUrl('StaticTable') }}" loading="lazy"
+                                                        onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';"
+                                                        alt="vector">
+                                                </div>
+                                            </div>
+                                            <div class="subsidiaries_details">
+                                                <div class="flags_sec align-items-start" >
+                                                    @foreach ($sub->investorAttributes as $row)
+                                                        <div class="flag_icon_titel">
+                                                            <div class="sub_contennt">
+                                                                <h6>
+                                                                    <img src="{{ App\Models\Country::where('id', $row->country_id)->first()->getFirstMediaUrl('flag') }}"
+                                                                        loading="lazy"
+                                                                        onerror="this.onerror=null;this.src='{{ asset('content/images/not-found/no-image.svg') }}';"
+                                                                        alt="vector">
+                                                                    <p>Since : <span
+                                                                            class="mt-0 pt-0">{{ $row->since }}</span>
+                                                                    </p>
+                                                                </h6>
+                                                                <h6>
+                                                                    <p>Sharing : <span
+                                                                            class="mt-0 pt-0">{{ $row->percent }}%</span>
+                                                                    </p>
+                                                                </h6>
+                                                            </div>
+
+                                                        </div>
+                                                    @endforeach
 
                                                 </div>
-                                            @endforeach
-
+                                            </div>
                                         </div>
-                                    </div>
-                                    </div>
-                                    <a href="{{ $sub->url }}" class="website_link mt-0 p-2 text-center">Website</a>
+                                        <a href="{{ $sub->url }}"
+                                            class="website_link mt-0 p-2 text-center">Website</a>
 
+                                    </div>
                                 </div>
-                            </div>
                             @empty
                                 @include('user.layout.includes.no-data')
                             @endforelse
@@ -214,21 +230,24 @@
             document.addEventListener('DOMContentLoaded', function() {
                 document.querySelectorAll('.nav-link').forEach(tab => {
                     tab.addEventListener('click', function() {
-                                               const type = this.getAttribute('data-id');
+                        const type = this.getAttribute('data-id');
 
                         // Dynamically construct the URL
                         let url = '{{ route('get-companies', ['type' => ':id']) }}'.replace(':id',
                             type);
-
+                        console.log(url);
+                        
                         // Make the AJAX request using fetch
                         fetch(url)
                             .then(response => response.json()) // Parse the JSON data
                             .then(data => {
-                                document.getElementById('sister').innerHTML = data.data;
+                                console.log(data);
+                                
+                                document.getElementById('container').innerHTML = data.data;
                                 // Here, you can use the data to dynamically update the UI
                             })
                             .catch(error => console.error('Error:', error)); // Log any errors
-                        
+
                     });
                 });
             });
